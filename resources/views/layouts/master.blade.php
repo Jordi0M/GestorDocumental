c1c5392dc8022049c2336ab3f78d9295d0fc6521
admingogs <jmartinezmateos@deloitte.es>
2019-03-06 11:35:46 +0100

    añadido css para saber si se puede editar o no

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,6 +5,8 @@
 
 	<!--JS propio-->
 	<script type="text/javascript" src="/js/script.js"></script>
+  <!--CSS propio-->
+  <link rel="stylesheet" type="text/css" href="/css/style.css">
 	<!--Font Awesome (iconos)-->
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 
