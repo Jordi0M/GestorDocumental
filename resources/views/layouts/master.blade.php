4075db69d7fed0ab46128ea6012b8425dc9031b2
Xusbcn <39081522+Xusbcn@users.noreply.github.com>
2019-03-12 08:12:56 +0100

    12/03/19

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -42,13 +42,15 @@
     @yield('script')
 
 	<style>
-	/*	.text {
+	/*
+  text {
   font-size:28px;
   font-family:helvetica;
   font-weight:bold;
   color:#71d90b;
   text-transform:uppercase;
-}*/
+}
+*/
 .parpadea {
   
   animation-name: parpadeo;
