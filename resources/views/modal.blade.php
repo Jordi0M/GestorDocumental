0af1550c98f3eb681c300f5e3651036e5e386594
Xusbcn <39081522+Xusbcn@users.noreply.github.com>
2019-03-14 10:05:26 +0100

    eliminacion de i class=fa fa-check chak-ok

--- resources/views/modal.blade.php
+++ resources/views/modal.blade.php
@@ -24,13 +24,13 @@
                 <div class="form-group">
                         <label for="nombre">Nombre</label>
                         <!--Admitiremos nombres de cualquier nacionalidad, incluyendo nombres compuestos y la mayoría de normas de acentuación. El número de caracteres será como mínimo 3 y como máximo 32.-->
-                        <input type="text" name="nombre" class="form-control " required pattern="[A-Za-z0-9]{5,40}" placeholder="Nombre..."><i class="fa fa-check check-ok"></i>
+                        <input type="text" name="nombre" class="form-control " required pattern="[A-Za-z0-9]{5,40}" placeholder="Nombre...">
                         
                 </div>
 
                 <div class="form-group">
                     <label for="documento">DNI/NIF</label>
-                    <input type="text" name="documento"  class="form-control" required pattern="^[A-Z|\d](-)?\d{7}[A-Z|\d]" placeholder="DNI/NIF..."><i class="fa fa-check check-ok"></i>
+                    <input type="text" name="documento"  class="form-control" required pattern="^[A-Z|\d](-)?\d{7}[A-Z|\d]" placeholder="DNI/NIF...">
                 </div>
             </div>
                 <!--------->
@@ -38,7 +38,7 @@
                 <div class="form-group">
                     <label for="direccion">Direccion</label>
                     <!--Sólo se permiten letras (mayúsculas y minúsculas) y números-->
-                    <input type="text" name="direccion" id="direcciones"class="form-control" minlength="5" maxlength="40" required pattern="[A-Za-z0-9]+" placeholder="Direccion..."><i class="fa fa-check check-ok"></i>
+                    <input type="text" name="direccion" id="direcciones"class="form-control" minlength="5" maxlength="40" required pattern="[A-Za-z0-9]+" placeholder="Direccion...">
                 </div>
             </div>
                 <!--------->
@@ -46,20 +46,20 @@
                 <div class="form-group">
                     <label for="provincia">Provincia</label>
                     <!--Sólo se permiten letras (mayúsculas y minúsculas)-->
-                    <input type="text" name="provincia" class="form-control" minlength="5" maxlength="40" required pattern="[A-Za-z]+" placeholder="Provincia..."><i class="fa fa-check check-ok"></i>
+                    <input type="text" name="provincia" class="form-control" minlength="5" maxlength="40" required pattern="[A-Za-z]+" placeholder="Provincia...">
                 </div>
         
         
                 <div class="form-group">
                         <label for="localidad">Localidad</label>
                         <!-- Sólo se permiten letras (mayúsculas y minúsculas)-->
-                        <input type="text" name="localidad" class="form-control" minlength="5" maxlength="40" required pattern="[A-Za-z]+" placeholder="Localidad..."><i class="fa fa-check check-ok"></i>
+                        <input type="text" name="localidad" class="form-control" minlength="5" maxlength="40" required pattern="[A-Za-z]+" placeholder="Localidad...">
                 </div>
 
                 <div class="form-group">
                     <label for="cp">Codigo Postal</label>
                     <!--Sólo se permiten números min:5-max:5-->
-                    <input type="text" name="cp" class="form-control" required pattern="^[0-5][1-9]{3}[0-9]$" placeholder="C.P..."><i class="fa fa-check check-ok"></i>
+                    <input type="text" name="cp" class="form-control" required pattern="^[0-5][1-9]{3}[0-9]$" placeholder="C.P...">
                 </div>
             </div>
             <!--------->
@@ -67,12 +67,12 @@
                 <div class="form-group">
                     <label for="telefono">Telefono</label>
                     <!--Sólo se permiten números con espacios y guiones-->
-                    <input type="text" name="telefono" id="telefonos"class="form-control" required pattern="^(\+34|0034|34)?[\s|\-|\.]?[6|7|8|9][\s|\-|\.]?([0-9][\s|\-|\.]?){8}$" placeholder="Telefono..."><i class="fa fa-check check-ok"></i>
+                    <input type="text" name="telefono" id="telefonos"class="form-control" required pattern="^(\+34|0034|34)?[\s|\-|\.]?[6|7|8|9][\s|\-|\.]?([0-9][\s|\-|\.]?){8}$" placeholder="Telefono...">
                 </div>
 
                 <div class="form-group">
                     <label for="mail">Mail</label>
-                    <input type="text" name="mail" class="form-control" required pattern="^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$" placeholder="Mail..."><i class="fa fa-check check-ok"></i>
+                    <input type="text" name="mail" class="form-control" required pattern="^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$" placeholder="Mail...">
                 </div>
             </div>
             <!--------->
