dad6ed701bead7752c874a24772e787733413cd1
jordi0m <jmartinezmateos@iesesteveterradas.cat>
2019-03-21 22:28:20 +0100

    cambio de http a https

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -33,7 +33,7 @@
     <script src="{{'https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js'}}"></script>
   	<script src="{{'https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js'}}"></script>
    
-    <script src="{{'http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js'}}"></script>  
+    <script src="{{'https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js'}}"></script>  
 
     @yield('script')
 
